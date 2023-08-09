16009e8db45a2374a6cc12fde99333ccac950e46
Misha <mishagrigoryan@gmail.com>
2023-08-08 23:47:02 -0400

    1

---
+++ fib.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    sequence = [0, 1]
+    while len(sequence) <= n:
+        next_value = sequence[-1] + sequence[-2]
+        sequence.append(next_value)
+    return sequence
+
+print(fibonacci(10))
